9bbfdf2048bc3fbb3c7a57c7cc65dbd40d21d4a8
Miguel Jarama <litoguemi_lw@hotmail.com>
2022-08-24 10:29:18 -0500

    local app first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BodyComponent } from './components/body/body.component';
+import { HttpClientModule } from "@angular/common/http"
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
