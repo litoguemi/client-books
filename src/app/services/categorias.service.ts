0268d0650bb8b36263ce71a9cde79a5ba5666f6e
Miguel Jarama <mjarama@ups.edu.ec>
2022-08-24 11:43:50 -0500

    Modificacion para que funcione con AWS

--- src/app/services/categorias.service.ts
+++ src/app/services/categorias.service.ts
@@ -6,7 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class CategoriasService {
 
-  url: any = 'http://localhost:8080/v1/categorias';
+  //url: any = 'http://localhost:8080/v1/categorias';
+  url: any = 'http://apirestbooksaws-env.eba-gznznjfp.us-east-1.elasticbeanstalk.com/v1/categorias';
 
   constructor(private http: HttpClient) {  }
 
